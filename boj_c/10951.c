a81c71e70d9e1f2b5ce45d49081a615aba12b291
return-to-the-lobby <97148865+return-to-the-lobby@users.noreply.github.com>
2023-01-25 18:33:00 +0900

    Create 10951.c

---
+++ boj_c/10951.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{	
+	int a, b;
+	while (scanf("%d %d", &a, &b) != EOF) {
+		printf("%d\n", a + b);
+	}
+	return 0;
+}
